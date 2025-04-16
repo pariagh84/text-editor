f5f0f0ef17242be05336724a44f8064df6080527
Paria Gharakhani <pariagharakhani@gmail.com>
2025-04-16 23:55:48 +0330

    complete FileManager class

--- src/editor/FileManager.java
+++ src/editor/FileManager.java
@@ -1,29 +1,61 @@
 package editor;
 
 import javax.swing.*;
+import java.io.*;
 
 public class FileManager {
     public static void openFile(TextEditor textEditor, JTextArea textArea) {
         JFileChooser fileChooser = new JFileChooser();
         if (fileChooser.showOpenDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-            //File file = fileChooser.getSelectedFile();
-            //TODO
+            File file = fileChooser.getSelectedFile();
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                textArea.setText("");
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    textArea.append(line + "\n");
+                }
+                textEditor.currentFile = file;
+                textEditor.setTitle("Simple Text Editor - " + file.getName());
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(textEditor,
+                        "Error reading file: " + e.getMessage(),
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE);
+            }
         }
     }
 
     public static void saveFile(TextEditor textEditor, JTextArea textArea) {
-        if (textEditor.currentFile == null){
+        if (textEditor.currentFile == null) {
             JFileChooser fileChooser = new JFileChooser();
             if (fileChooser.showSaveDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-                //TODO
+                File file = fileChooser.getSelectedFile();
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+                    writer.write(textArea.getText());
+                    textEditor.currentFile = file;
+                    textEditor.setTitle("Simple Text Editor - " + file.getName());
+                } catch (IOException e) {
+                    JOptionPane.showMessageDialog(textEditor,
+                            "Error saving file: " + e.getMessage(),
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                }
             }
-        }
-        else{
-            //TODO
+        } else {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(textEditor.currentFile))) {
+                writer.write(textArea.getText());
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(textEditor,
+                        "Error saving file: " + e.getMessage(),
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE);
+            }
         }
     }
 
     public static void newFile(TextEditor textEditor, JTextArea textArea) {
-        //TODO
+        textArea.setText("");
+        textEditor.currentFile = null;
+        textEditor.setTitle("Simple Text Editor");
     }
 }
